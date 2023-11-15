4ef35dd95d6e383fca88ac77b5f864e61272b28b
DedAzaMarks <maxmutant1@yandex.ru>
2023-11-15 17:17:41 +0300

    Solve task: stdmacro

--- stdmacro/src/lib.rs
+++ stdmacro/src/lib.rs
@@ -2,15 +2,40 @@
 
 #[macro_export]
 macro_rules! deque {
-    // TODO: your code here.
+    () => {
+        __VecDeque::new()
+    };
+    ($($x:expr),+) => {
+        __VecDeque::from([$($x), +])
+    };
+    ($elem:expr; $n:expr) => {
+        __VecDeque::from([$elem; $n])
+    };
 }
 
 #[macro_export]
 macro_rules! sorted_vec {
-    // TODO: your code here.
+    () => {
+        Vec::new()
+    };
+    ($($x:expr), +) => {
+        {
+            let mut res = Vec::from([$($x), +]);
+            res.sort();
+            res
+        }
+    };
 }
 
 #[macro_export]
 macro_rules! map {
-    // TODO: your code here.
+    () => {
+        __HashMap::new()
+    };
+    ($($x:expr => $y:expr), +) => {
+        __HashMap::from([$(($x, $y)), +])
+    };
+    ($($x:expr => $y:expr), +, $(, )?) => {
+        __HashMap::from([$(($x, $y)), +])
+    }
 }
