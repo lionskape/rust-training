84531bcf4c4869e1676f884d10af59655a75fb60
DedAzaMarks <maxmutant1@yandex.ru>
2023-11-24 22:29:06 +0300

    Solve task: tcp_proxy

--- tcp_proxy/src/lib.rs
+++ tcp_proxy/src/lib.rs
@@ -3,10 +3,20 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-use log::info;
-use std::io::prelude::*;
+use std::io::copy;
 
 pub fn run_proxy(port: u32, destination: String) {
-    // TODO: your code here.
-    unimplemented!()
+    let lis = TcpListener::bind(format!("localhost:{}", port)).unwrap();
+    for conn in lis.incoming() {
+        let mut c1 = conn.unwrap();
+        let mut s1 = TcpStream::connect(destination.as_str()).unwrap();
+        let mut c2 = c1.try_clone().unwrap();
+        let mut s2 = s1.try_clone().unwrap();
+        thread::spawn(move || {
+            copy(&mut c1, &mut s1).unwrap();
+        });
+        thread::spawn(move || {
+            copy(&mut s2, &mut c2).unwrap();
+        });
+    }
 }
